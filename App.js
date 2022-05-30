1344cd9776aaa7826e5c50d55e4eb3074ce516e4
= <roberluna8@gamil.com>
2022-05-30 17:54:24 -0600

    Mi prmier commit

--- App.js
+++ App.js
@@ -1,12 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, ScrollView} from 'react-native';
+
+const logo = {
+  uri: "https://reactnative.dev/img/tiny_logo.png",
+  width: 64,
+  height: 64
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ScrollView>
       <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
